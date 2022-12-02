150f443e5b837b1e888fa81bb241cfdc5541bd61
saawd-wold <saawd@woldinghamschool.co.uk>
2022-12-02 07:52:08 +0000

    Added numpy prerequisite

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ __version__ = "0.0.13"      #
 __name__    = "sixthformds" #
 __author__  = "Daniel Sääw" #
 #===========================#
+# Add any libraries your code needs to the list below!
+__prereqs__ = ["numpy"]
 
 print(f"Setting up package {__name__}v{__version__} by {__author__}.")
 
@@ -14,5 +16,6 @@ setup(
     version=__version__,
     description="A simple library of naively implemented data structures.",
     author=__author__,
-    packages=find_packages()
+    packages=find_packages(),
+    requires=__prereqs__,
 )
